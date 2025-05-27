dc3c93610b0c8b7f4c073e41625cce651bc25e17
sundalei <sundalei1988@gmail.com>
2025-05-27 20:01:32 +0800

    Update index script

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    console.log(core);
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
 } catch (error) {
-    console.log(error);
+    core.setFailed(error.message);
 }
-console.log("Hello World");
