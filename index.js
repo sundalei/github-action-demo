b4b57d12e76a2472b60724a02859232928674ca2
sundalei <sundalei1988@gmail.com>
2025-05-27 18:39:09 +0800

    Update index script

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    console.log(core);
+} catch (error) {
+    console.log(error);
+}
 console.log("Hello World");
